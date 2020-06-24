a61933c2027bf6769e815e6e11c1936320e05152
PiyushGedekar <pgedekar@gmail.com>
2020-06-24 17:33:22 +0530

    Star patterns

---
+++ src/test/java/Display/Alphabets.java
@@ -0,0 +1,27 @@
+package Display;
+
+public class Alphabets {
+	public static void main(String[] args){
+		for(int i=1;i<5;i++)
+		{
+			int alpha=65;
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print((char)alpha);
+				alpha++;
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+/*
+  Output==
+A
+AB
+ABC
+ABCD
+  
+  */
+ 
---
+++ src/test/java/Display/Alphabets02.java
@@ -0,0 +1,27 @@
+package Display;
+
+public class Alphabets02 {
+	public static void main(String[] args){
+		
+		int alpha=65;
+		
+		for(int i=1;i<5;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print((char)alpha);
+				alpha++;
+			}
+			System.out.println();
+		}
+	}
+
+}
+/*
+Output==
+A
+BC
+DEF
+GHIJ
+
+*/
